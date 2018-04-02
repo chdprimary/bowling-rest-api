eef435cd679d876b42e0bfa8aa4912def383af01
Christian Dean <chdprimary@gmail.com>
2018-04-02 09:37:28 -0500

    Placed constraints on # players, length of names

--- models.py
+++ models.py
@@ -2,15 +2,10 @@ from mongoengine import *
 
 connect('bowlapp_mongodb')
 
-# Supporting 2+ players
-# players will each have a list of rolls
-# Frames aren't needed if each frame is a constant length of 2
-# - strike frame can insert a "buffer" roll of 0 in 2nd slot
 class Player(EmbeddedDocument):
-    name = StringField(required=True)
+    name = StringField(min_length=1, max_length=25, required=True)
     rolls = ListField(IntField())
 
-# Game will track whether finished or not, and players
 class Game(Document):
     finished = BooleanField(default=False)
-    players = ListField(EmbeddedDocumentField(Player), required=True)
+    players = ListField(EmbeddedDocumentField(Player), min_length=1, max_length=4, required=True)
